b8abd384963c776448734de3adf199ff682dc9ea
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-01-25 13:46:24 -0500

    💄  Add styles for 375px screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+      },
+      fontSize: {
+        project: ["13px"],
+      },
+      colors: {
+        veryDarkGrayishBlue: "hsl(217, 19%, 35%)",
+        desaturatedDarkBlue: "hsl(214, 17%, 51%)",
+        grayishBlue: "hsl(212, 23%, 69%)",
+        lightGrayishBlue: "hsl(210, 46%, 95%)",
+      }
+    },
   },
   plugins: [],
-}
+};
