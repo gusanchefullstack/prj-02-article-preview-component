f87f070550ab3bf0fbbd0a10b8b0a2034a9ec074
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-01-26 16:28:19 -0500

    🐛  Fix for states management in desktop and mobile

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ const shareButtonCard = document.getElementById("share-button-card");
 const shareButtonSocial = document.getElementById("share-button-social");
 
 shareButtonCard.addEventListener("click", (ev) => {
-  contactCard.classList.replace("flex", "hidden");
+  
   shareBanner.classList.replace("hidden", "flex");
 });
 shareButtonSocial.addEventListener("click", (ev) => {
-  contactCard.classList.replace("hidden", "flex");
+  
   shareBanner.classList.replace("flex", "hidden");
 });
