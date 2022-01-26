f43868889eb151357d3e311d73fc5474f898996e
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-01-25 19:17:30 -0500

    💄  Add states for mobile screen

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-import './style.css'
+import "./style.css";
+
+const contactCard = document.getElementById("contact-card");
+const shareBanner = document.getElementById("share-banner");
+const shareButtonCard = document.getElementById("share-button-card");
+const shareButtonSocial = document.getElementById("share-button-social");
+
+shareButtonCard.addEventListener("click", (ev) => {
+  contactCard.classList.replace("flex", "hidden");
+  shareBanner.classList.replace("hidden", "flex");
+});
+shareButtonSocial.addEventListener("click", (ev) => {
+  contactCard.classList.replace("hidden", "flex");
+  shareBanner.classList.replace("flex", "hidden");
+});
