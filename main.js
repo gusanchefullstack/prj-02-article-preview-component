90d4f9516b82e5dc4a09204e6012424b9b0013a6
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-01-26 17:30:03 -0500

    💄  Fix states of banner

--- main.js
+++ main.js
@@ -3,13 +3,38 @@ import "./style.css";
 const contactCard = document.getElementById("contact-card");
 const shareBanner = document.getElementById("share-banner");
 const shareButtonCard = document.getElementById("share-button-card");
+const shareButtonCardImg = document.getElementById("share-button-card-img");
 const shareButtonSocial = document.getElementById("share-button-social");
+let bannerDisplayed = false;
 
 shareButtonCard.addEventListener("click", (ev) => {
-  
-  shareBanner.classList.replace("hidden", "flex");
+  if (!bannerDisplayed) {
+    shareBanner.classList.replace("hidden", "flex");
+    bannerDisplayed = true;
+    shareButtonCard.classList.replace(
+      "bg-lightGrayishBlue",
+      "bg-desaturatedDarkBlue"
+    );
+    shareButtonCardImg.setAttribute("src", "images/icon-share-grey.svg");
+  } else {
+    shareBanner.classList.replace("flex", "hidden");
+    bannerDisplayed = false;
+    shareButtonCard.classList.replace(
+      "bg-desaturatedDarkBlue",
+      "bg-lightGrayishBlue"
+    );
+    shareButtonCardImg.setAttribute("src", "images/icon-share.svg");
+  }
 });
+
 shareButtonSocial.addEventListener("click", (ev) => {
-  
-  shareBanner.classList.replace("flex", "hidden");
+  if (bannerDisplayed) {
+    shareBanner.classList.replace("flex", "hidden");
+    bannerDisplayed = false;
+    shareButtonCard.classList.replace(
+      "bg-desaturatedDarkBlue",
+      "bg-lightGrayishBlue"
+    );
+    shareButtonCardImg.setAttribute("src", "images/icon-share.svg");
+  }
 });
